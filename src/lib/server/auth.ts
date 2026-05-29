0d6fdd726897eb3d7d1ab04fa31e49ce7170c898
Noman <pxycknomdictator@outlook.com>
2026-05-29 16:58:47 +0500

    add email verification during signup

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -4,6 +4,7 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { getRequestEvent } from "$app/server";
+import { sendEmail } from "./email/resend";
 import { admin, username, lastLoginMethod } from "better-auth/plugins";
 
 export const auth = betterAuth({
@@ -12,5 +13,15 @@ export const auth = betterAuth({
 	secret: env.BETTER_AUTH_SECRET,
 	database: drizzleAdapter(db, { provider: "pg", usePlural: true, transaction: true }),
 	emailAndPassword: { enabled: true, requireEmailVerification: true },
+	emailVerification: {
+		sendOnSignUp: true,
+		sendVerificationEmail: async ({ user, url }) => {
+			void sendEmail({
+				receiver: user.email,
+				subject: "Verify your email address",
+				text: `Hey ${user.name} Click the link to verify your email: ${url}`
+			});
+		}
+	},
 	plugins: [admin(), username(), lastLoginMethod(), sveltekitCookies(getRequestEvent)]
 });
