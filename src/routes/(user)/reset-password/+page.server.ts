dc25c83056db89e2b203aca8a8e374634447c092
Noman <pxycknomdictator@outlook.com>
2026-06-05 07:34:46 +0500

    change password schema to reset password schema

--- src/routes/(user)/reset-password/+page.server.ts
+++ src/routes/(user)/reset-password/+page.server.ts
@@ -1,9 +1,9 @@
 import { superValidate } from "sveltekit-superforms";
 import { zod4 } from "sveltekit-superforms/adapters";
 import type { PageServerLoad } from "./$types";
-import { passwordSchema } from "$lib/validator/auth-validator";
+import { resetPasswordSchema } from "$lib/validator/auth-validator";
 
 export const load: PageServerLoad = async () => {
-	const form = await superValidate(zod4(passwordSchema));
+	const form = await superValidate(zod4(resetPasswordSchema));
 	return { form };
 };
