538c4513763b984f51cb72d21563a8f7578eb06e
Noman <pxycknomdictator@outlook.com>
2026-06-09 03:06:14 +0500

    add user logout feature

--- src/routes/(user)/dashboard/+page.svelte
+++ src/routes/(user)/dashboard/+page.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
+	import { authClient } from "$lib/auth-client";
+
 	let { data } = $props();
+
+	async function logout() {
+		const { error } = await authClient.signOut();
+
+		if (error) {
+			console.error(error.message || "unexpected error");
+			return;
+		}
+
+		await goto("/login", { replaceState: true, invalidateAll: true });
+	}
 </script>
 
 <main>
 	{#if data}
 		<h1>Welcome {data.name}</h1>
 		<span>Session Id: {data.session}</span>
+		<div>
+			<button type="button" onclick={logout}>logout</button>
+		</div>
 	{/if}
 </main>
