4f3284af0296ca9860294712dc864301117975aa
Noman <pxycknomdictator@outlook.com>
2026-06-08 15:53:55 +0500

    add forget password server action

--- src/routes/(user)/forget-password/+page.server.ts
+++ src/routes/(user)/forget-password/+page.server.ts
@@ -1,9 +1,21 @@
-import { superValidate } from "sveltekit-superforms";
+import { fail, superValidate } from "sveltekit-superforms";
 import { zod4 } from "sveltekit-superforms/adapters";
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { emailSchema } from "$lib/validator/auth-validator";
 
 export const load: PageServerLoad = async () => {
 	const form = await superValidate(zod4(emailSchema));
 	return { form };
+};
+
+export const actions: Actions = {
+	async forget_password({ request }) {
+		const form = await superValidate(request, zod4(emailSchema));
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		console.log(form.data);
+	}
 };
