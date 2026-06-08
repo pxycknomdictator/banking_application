5a71c25a25d82646afb531dd355f40b895ce26bf
Noman <pxycknomdictator@outlook.com>
2026-06-08 20:25:19 +0500

    add verify email page

---
+++ src/routes/(user)/verify-email/+page.svelte
@@ -0,0 +1,8 @@
+<main>
+	<section>
+		<h1>Check your inbox</h1>
+		<form action="?/resend_email" method="post">
+			<button type="submit">Resend Verification Email</button>
+		</form>
+	</section>
+</main>
