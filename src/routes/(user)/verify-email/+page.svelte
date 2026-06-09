db2366bd50d23c8e509ca1db59f9af86d55d4454
Noman <pxycknomdictator@outlook.com>
2026-06-09 15:11:57 +0500

    add verification page content

--- src/routes/(user)/verify-email/+page.svelte
+++ src/routes/(user)/verify-email/+page.svelte
@@ -1,8 +1,17 @@
 <main>
 	<section>
 		<h1>Check your inbox</h1>
+		<div>
+			<p>
+				We've sent a verification link to your email address. Please click the link in the
+				email to verify your account.
+			</p>
+		</div>
 		<form action="?/resend_email" method="post">
 			<button type="submit">Resend Verification Email</button>
 		</form>
+		<div>
+			<p>Didn't get the email? Check your spam folder.</p>
+		</div>
 	</section>
 </main>
