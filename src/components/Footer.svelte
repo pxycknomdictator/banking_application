c6888fde717413a2f0e9d484962e15bc07527b58
Noman <pxycknomdictator@outlook.com>
2026-06-14 20:58:05 +0500

    add responsiveness in footer component

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,14 +6,16 @@
 	let { linkTexts }: Props = $props();
 </script>
 
-<footer class="flex h-20 w-full items-center border border-outline px-16">
-	<div class="flex w-full items-center justify-between">
+<footer class="flex h-20 w-full items-center border border-outline px-6 md:px-16">
+	<div
+		class="flex w-full flex-col justify-center gap-y-1 sm:flex-row sm:justify-between sm:gap-y-0"
+	>
 		<div>
-			<h6>© 2026 Banking Application. All rights reserved.</h6>
+			<h6 class="text-[13px] sm:text-[14px]">© 2026 Banking Application</h6>
 		</div>
-		<nav class="flex list-none items-center gap-x-8">
+		<nav class="flex list-none items-center gap-x-8 sm:gap-x-8">
 			{#each linkTexts as text (text)}
-				<li>{text}</li>
+				<li class="text-[13px] sm:text-[14px]">{text}</li>
 			{/each}
 		</nav>
 	</div>
